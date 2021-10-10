9cdda05ee1556d2327afe5b678a139022f802316
Leoš Gjumija <gjumle@protonmail.com>
2021-10-10 17:04:48 +0200

    Finished
    Without the disounts

--- projects/taxes/script.js
+++ projects/taxes/script.js
@@ -14,12 +14,16 @@ function calculate() {
 	var in_out_d = inc - exp;
 	var tax_base_s = in_out_d + n_tax + ins;
 	var ed_tax_base_r = tax_base_s + gft - ls;
+	var f_tax = ed_tax_base_r * 0.15;
 
 	console.log(in_out_d);
 	console.log(tax_base_s);
 	console.log(ed_tax_base_r);
+	console.log(f_tax);
 
 	document.getElementById('in_out_result').innerHTML = inc + " - " + exp + " = " + in_out_d;
 	document.getElementById('tax_base_result').innerHTML = in_out_d + " + " + n_tax + " + " + ins + " = " + tax_base_s;
 	document.getElementById('edited_tax_base_result').innerHTML = tax_base_s + " + " + gft + " - " + ls + " = " + ed_tax_base_r;
+
+	document.getElementById('result').innerHTML = ed_tax_base_r + " * 0.15 = " + f_tax;
 }
