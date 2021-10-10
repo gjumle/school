afc3ea2582e7cf3a41f06b2cf7be17d9cc3cdca7
Leoš Gjumija <gjumle@protonmail.com>
2021-10-10 16:19:55 +0200

    Remake
    All I needed was to multiply the input by one to get integer.

--- projects/taxes/script.js
+++ projects/taxes/script.js
@@ -1,11 +1,16 @@
-function difference(x, y) {
-	return x - y;
-}
+function calculate() {
+	var inc = document.getElementById('incomes').value * 1;
+	var exp = document.getElementById('expenses').value * 1;
 
+	var n_tax = document.getElementById('non-taxable').value * 1;
+	var ins = document.getElementById('insurance').value * 1;
 
-function in_out() {
-	var inc = document.getElementById('incomes').value;
-	var exp = document.getElementById('expenses').value;
+	var in_out_d = inc - exp;
+	var tax_base_s = in_out_d + n_tax + ins;
 
-	document.getElementById('result').innerHTML = difference(inc, exp);
+	console.log(in_out_d);
+	console.log(tax_base_s);
+
+	document.getElementById('in_out_result').innerHTML = inc + " - " + exp + " = " + in_out_d;
+	document.getElementById('tax_base').innerHTML = in_out_d + " + " + n_tax + " + " + ins + " = " + tax_base_s;
 }
