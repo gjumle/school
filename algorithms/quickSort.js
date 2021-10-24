0e927364742d7f8047311715686d9a71c3db9b82
Leoš Gjumija <gjumle@protonmail.com>
2021-10-24 13:43:55 +0200

    Update quickSort.js

--- algorithms/quickSort.js
+++ algorithms/quickSort.js
@@ -1,3 +1,19 @@
-function quickSort(arr) {
-    
+function partition(arr, left, right) {
+    let pivot = arr[Math.floor((left + right) / 2)]
+    i = left;
+    j = right;
+    while (i <= j) {
+        while (arr[i] < pivot) {
+            i++;
+        }
+        while (arr[j] > pivot) {
+            j--;
+        }
+        if (i <= j) {
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+            i++;
+            j--;
+        }
+    }
+    return i;
 }
