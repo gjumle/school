b71e114174414b597ce8aa38b7c4a4c49d9d162d
Leoš Gjumija <gjumle@protonmail.com>
2022-01-12 20:20:37 +0100

    Update ministri.sql

--- sql/data_grip/ministri.sql
+++ sql/data_grip/ministri.sql
@@ -4,16 +4,29 @@ USE
 
 CREATE TABLE poslanci
 (
-    p_id     INT     NOT NULL AUTO_INCREMENT,
-    jmeno    VARCHAR NOT NULL DEFAULT '',
-    prijmeni VARCHAR NOT NULL DEFAULT '',
+    p_id     INT         NOT NULL AUTO_INCREMENT,
+    jmeno    VARCHAR(25) NOT NULL DEFAULT '',
+    prijmeni VARCHAR(25) NOT NULL DEFAULT '',
     PRIMARY KEY (p_id)
-)
+);
 
 
 CREATE TABLE ministerstva
 (
-    m_id  INT     NOT NULL AUTO_INCREMENT,
-    nazev VARCHAR NOT NULL DEFAULT '',
+    m_id  INT         NOT NULL AUTO_INCREMENT,
+    nazev VARCHAR(25) NOT NULL DEFAULT '',
     PRIMARY KEY (m_id)
-)
+);
+
+
+CREATE TABLE funkcni_obdobi
+(
+    poslanec_id INT  NOT NULL DEFAULT 0,
+    ministr_id  INT  NOT NULL DEFAULT 0,
+    od          DATE NOT NULL DEFAULT '0000-00-00',
+    do          DATE NOT NULL DEFAULT '0000-00-00',
+    CONSTRAINT p2f FOREIGN KEY (poslanec_id) REFERENCES poslanci (p_id),
+    CONSTRAINT m2f FOREIGN KEY (poslanec_id) REFERENCES ministerstva (m_id)
+);
+
+
