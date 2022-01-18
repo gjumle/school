ccceb031c1eed0ddd0c68c6ae36d16e41d246042
Leoš Gjumija <gjumle@protonmail.com>
2022-01-18 08:55:15 +0100

    Update sales.sql

--- sql/data_grip/sales.sql
+++ sql/data_grip/sales.sql
@@ -102,3 +102,142 @@ BEGIN
     INSERT INTO mv_sales_stats
     VALUES (NEW.product_name, @new_price_sum, @new_amount_sum, @new_price_avg, @new_amount_avg, @new_sales_cnt);
 END;
+
+
+CREATE TRIGGER T_AD_sales
+    AFTER DELETE
+    ON sales
+    FOR EACH ROW
+BEGIN
+    SELECT price_sum, amount_sum, price_avg, amount_avg, sales_cnt
+    FROM mv_sales_stats
+    WHERE product_name = OLD.product_name
+    INTO @old_price_sum, @old_amount_sum, @old_price_avg, @old_amount_avg, @old_sales_cnt;
+
+    SET @new_price_sum = IFNULL(@old_price_sum, 0) - IFNULL(OLD.product_price, 0);
+    SET @new_amount_sum = IFNULL(@old_amount_sum, 0) - IFNULL(OLD.product_amount, 0);
+    SET @new_sales_cnt = IFNULL(@old_sales_cnt, 0) - 1;
+    SET @new_price_avg = @new_price_sum / @new_amount_sum;
+    SET @new_amount_avg = @new_amount_sum / @new_sales_cnt;
+
+    DELETE FROM mv_sales_stats WHERE product_name = OLD.product_name;
+    INSERT INTO mv_sales_stats
+    VALUES (OLD.product_name, @new_price_sum, @new_amount_sum,
+            IFNULL(@new_price_avg, 0), IFNULL(@new_amount_avg, 0), @new_sales_cnt);
+END;
+
+
+CREATE TRIGGER T_AU_sales
+    AFTER UPDATE
+    ON sales
+    FOR EACH ROW
+BEGIN
+    SELECT price_sum, amount_sum, price_avg, amount_avg, sales_cnt
+    FROM mv_sales_stats
+    WHERE product_name = OLD.product_name
+    INTO @old_price_sum, @old_amount_sum, @old_price_avg, @old_amount_avg, @old_sales_cnt;
+
+    SET @new_price_sum = IFNULL(@old_price_sum, 0) + (NEW.product_price - IFNULL(OLD.product_price, 0));
+    SET @new_amount_sum = IFNULL(@old_amount_sum, 0) + (NEW.product_amount - IFNULL(OLD.product_amount, 0));
+    SET @new_sales_cnt = IFNULL(@old_sales_cnt, 0);
+    SET @new_price_avg = @new_price_sum / @new_sales_cnt;
+    SET @new_amount_avg = @new_amount_sum / @new_sales_cnt;
+
+    DELETE FROM mv_sales_stats WHERE product_name = OLD.product_name;
+    INSERT INTO mv_sales_stats
+    VALUES (OLD.product_name, @new_price_sum, @new_amount_sum,
+            IFNULL(@new_price_avg, 0), IFNULL(@new_amount_avg, 0), @new_sales_cnt);
+END;
+
+
+CREATE PROCEDURE P_trigger_sales(
+    IN trigger_type VARCHAR(25),
+    IN new_product_name VARCHAR(25),
+    IN new_product_price DECIMAL(10, 2),
+    IN new_product_amount DECIMAL(8, 2),
+    IN old_product_name VARCHAR(25),
+    IN old_product_price DECIMAL(10, 2),
+    IN old_product_amount DECIMAL(8, 2)
+)
+BEGIN
+    IF (trigger_type = 'ai') THEN
+        SELECT price_sum, amount_sum, price_avg, amount_avg, sales_cnt
+        FROM mv_sales_stats
+        WHERE product_name = new_product_name
+        INTO @old_price_sum, @old_amount_sum, @old_price_avg, @old_amount_avg, @old_sales_cnt;
+
+        SET @new_price_sum = IFNULL(@old_price_sum, 0) + new_product_price;
+        SET @new_amount_sum = IFNULL(@old_amount_sum, 0) + new_product_amount;
+        SET @new_sales_cnt = IFNULL(@old_sales_cnt, 0) + 1;
+        SET @new_price_avg = @new_price_sum / @new_sales_cnt;
+        SET @new_amount_avg = @new_amount_sum / @new_sales_cnt;
+
+        DELETE FROM mv_sales_stats WHERE product_name = new_product_name;
+        INSERT INTO mv_sales_stats
+        VALUES (new_product_name, @new_price_sum, @new_amount_sum, @new_price_avg, @new_amount_avg, @new_sales_cnt);
+    END IF;
+    IF (trigger_type = 'au') THEN
+        SELECT price_sum, amount_sum, price_avg, amount_avg, sales_cnt
+        FROM mv_sales_stats
+        WHERE product_name = new_product_name
+        INTO @old_price_sum, @old_amount_sum, @old_price_avg, @old_amount_avg, @old_sales_cnt;
+
+        SET @new_price_sum = IFNULL(@old_price_sum, 0) + (new_product_price - IFNULL(old_product_price, 0));
+        SET @new_amount_sum = IFNULL(@old_amount_sum, 0) + (new_product_amount - IFNULL(old_product_amount, 0));
+        SET @new_sales_cnt = IFNULL(@old_sales_cnt, 0);
+        SET @new_price_avg = @new_price_sum / @new_sales_cnt;
+        SET @new_amount_avg = @new_amount_sum / @new_sales_cnt;
+
+        DELETE FROM mv_sales_stats WHERE product_name = old_product_name;
+        INSERT INTO mv_sales_stats
+        VALUES (old_product_name, @new_price_sum, @new_amount_sum,
+                IFNULL(@new_price_avg, 0), IFNULL(@new_amount_avg, 0), @new_sales_cnt);
+    END IF;
+    IF (trigger_type = 'ad') THEN
+        SELECT price_sum, amount_sum, price_avg, amount_avg, sales_cnt
+        FROM mv_sales_stats
+        WHERE product_name = old_product_name
+        INTO @old_price_sum, @old_amount_sum, @old_price_avg, @old_amount_avg, @old_sales_cnt;
+
+        SET @new_price_sum = IFNULL(@old_price_sum, 0) - IFNULL(old_product_price, 0);
+        SET @new_amount_sum = IFNULL(@old_amount_sum, 0) - IFNULL(old_product_amount, 0);
+        SET @new_sales_cnt = IFNULL(@old_sales_cnt, 0) - 1;
+        SET @new_price_avg = @new_price_sum / @new_amount_sum;
+        SET @new_amount_avg = @new_amount_sum / @new_sales_cnt;
+
+        DELETE FROM mv_sales_stats WHERE product_name = old_product_name;
+        INSERT INTO mv_sales_stats
+        VALUES (old_product_name, @new_price_sum, @new_amount_sum,
+                IFNULL(@new_price_avg, 0), IFNULL(@new_amount_avg, 0), @new_sales_cnt);
+    END IF;
+END;
+
+
+CREATE TRIGGER T_P_AI_sales
+    AFTER INSERT
+    ON sales
+    FOR EACH ROW
+BEGIN
+    CALL P_trigger_sales('ai', NEW.product_name, NEW.product_price, NEW.product_amount,
+                         NULL, NULL);
+END;
+
+
+CREATE TRIGGER T_P_AU_sales
+    AFTER UPDATE
+    ON sales
+    FOR EACH ROW
+BEGIN
+    CALL P_trigger_sales('au', NEW.product_name, NEW.product_price, NEW.product_amount, OLD.product_name,
+                         OLD.product_price, OLD.product_price);
+END;
+
+
+CREATE TRIGGER T_P_AD_sales
+    AFTER DELETE
+    ON sales
+    FOR EACH ROW
+BEGIN
+    CALL P_trigger_sales('ad', NULL, NULL, NULL, OLD.product_name,
+                         OLD.product_price, OLD.product_price);
+END;
