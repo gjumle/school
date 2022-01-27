26f003320e084a04b92c2c36b706d796bdaceeef
Leoš Gjumija <gjumle@protonmail.com>
2022-01-27 10:25:00 +0100

    Update names.php

--- php/names.php
+++ php/names.php
@@ -1,16 +1,18 @@
 <?php
-$names = array("Terka", "Petr", "Lukas", "Daniel", "Simon");
+$names = array("Terka", "Petr", "Lukas", "Daniel", "Simon", "Petr", "Karel");
 
-function name_count($arr) {
-    global  $names;
+function name_count($name) {
+    global $names;
     $i = 0;
     $ret = 0;
-    while ($i <= count($arr)-1) {
-        if ($arr == $names[$i]) {
+    $arr_len = count($names)-1;
+    while ($i <= $arr_len) {
+        if ($names[$i] == $name) {
             $ret += 1;
         }
+        $i += 1;
     }
-    return "Pocet jmen" . $ret;
+    return "Pocet jmen " . $name . " je v seznamu " . $ret . " krat.";
 }
 
-echo name_count($names);
+echo name_count("Karel");
