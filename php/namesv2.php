b97953f738e9c2d20f3a18bab468fd51e480f6c5
Leoš Gjumija <gjumle@protonmail.com>
2022-01-27 10:50:24 +0100

    Update namesv2.php

--- php/namesv2.php
+++ php/namesv2.php
@@ -1,11 +1,12 @@
 <?php
-$names = array("Terka", "Petr", "Lukas", "Daniel", "Simon", "Petr", "Karel");
+$names = array("Terka", "Petr", "Lukas", "Daniel", "Simon", "Petr", "Karel", "Lukas");
 
 function names_count($arr) {
     $compare = array_unique($arr);
     $i = 0;
     $ret = array();
     $count = 0;
+    $tret = array();
     foreach ($arr as $i) {
         foreach ($compare as $j) {
             if ($i == $j) {
@@ -15,7 +16,11 @@ function names_count($arr) {
         $ret[] = $count;
         $count = 0;
     }
-    return implode(" ", $ret);
+    foreach ($compare as $y) {
+        $tret = $compare[$y] . " x " . $ret[$y];
+        $y += 1;
+    }
+    return implode(" ", $tret);
 }
 
 echo names_count($names);
