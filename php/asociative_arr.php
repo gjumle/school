1172272328eafbf2dd4b7c3d5022bce302559296
Leoš Gjumija <gjumle@protonmail.com>
2022-01-31 22:07:51 +0100

    Update asociative_arr.php

--- php/asociative_arr.php
+++ php/asociative_arr.php
@@ -2,7 +2,7 @@
 
 $age = array("Petr"=>"26", "Daniel"=>"18", "Lukas"=>"16");
 
-foreach ($age as $x => $x_value) {
-    echo "Key=" . $x . ", Value=" . $x_value;
+foreach ($age as $x_key => $x_value) {
+    echo "Key=" . $x_key . ", Value=" . $x_value;
     echo "<br>";
 }
