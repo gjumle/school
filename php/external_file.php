3ef8eda83750f1c9fb7234f551557414a9d0e073
Leoš Gjumija <gjumle@protonmail.com>
2022-02-03 09:50:50 +0100

    Update external_file.php

--- php/external_file.php
+++ php/external_file.php
@@ -1,15 +1,25 @@
 <?php
     $jmeno = isset($_POST["jmeno"]) ? $_POST["jmeno"] : "";
     $text = isset($_POST["text"]) ? $_POST["text"] : "";
-    $time = date("h:i:sa");
+    $time = date("Y-m-d H:i:s");
 
     function save_data($jmeno, $text) {
         global $jmeno, $text, $time;
-        $data = fopen("data.txt", "w");
+        $data = fopen("data.txt", "w+");
         fputs($data, $jmeno . "#" . $text . "#" . $time . "#");
         fclose($data);
     }
     save_data($jmeno, $text);
+
+    function show_data() {
+        $content = file_get_contents("data.txt");
+        $clear_content = explode("#", $content);
+        foreach ($clear_content as $item) {
+             echo "Uzivatel ". $clear_content[0] . " napsal " . $clear_content[1]. " v " . $clear_content[2]. ".<br>";
+        }
+    }
+
+    echo show_data();
 ?>
 
 <html lang="en">
