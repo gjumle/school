816bdf4822946a65725eb2e5510d8978be9d4448
Leoš Gjumija <gjumle@protonmail.com>
2022-02-16 10:40:02 +0100

    Create index.php

---
+++ dev/db_conn/index.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+
+$mysqli = new mysqli($servername, $username, $password);
+
+if ($mysqli->connect_errno) {
+    printf("Connect failed: %s<br />", $mysqli->connect_error);
+    exit();
+}
+printf('Connected successfully.<br />');
+
+if ($mysqli->query("CREATE DATABASE")) {
+    printf("Database created successfully.<br />");
+}
+if ($mysqli->errno) {
+    printf("Could not create database: %s<br />", $mysqli->error);
+}
+
+$mysqli->close();
+
+
