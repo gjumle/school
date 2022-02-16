7ce12ecb86311345cc12d4a882e4371d77c8ebf8
Leoš Gjumija <gjumle@protonmail.com>
2022-02-16 12:49:07 +0100

    Update index.php

--- dev/db_conn/index.php
+++ dev/db_conn/index.php
@@ -1,23 +1,72 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "root";
+$password = "";
+$db = "myDB";
 
-$mysqli = new mysqli($servername, $username, $password);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
 
-if ($mysqli->connect_errno) {
-    printf("Connect failed: %s<br />", $mysqli->connect_error);
-    exit();
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-printf('Connected successfully.<br />');
+echo "Connected successfully. <br>";
 
-if ($mysqli->query("CREATE DATABASE")) {
-    printf("Database created successfully.<br />");
+// Create database
+$sql = "CREATE DATABASE IF NOT EXISTS myDB";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully. <br>";
+} else {
+    echo "Error creating database: " . $conn->error;
 }
-if ($mysqli->errno) {
-    printf("Could not create database: %s<br />", $mysqli->error);
+
+
+// Create table
+$sql = "CREATE TABLE IF NOT EXISTS MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)";
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully. <br>";
+} else {
+    echo "Error creating table: " . $conn->error;
 }
 
-$mysqli->close();
+// Insert data
+$sql = "REPLACE INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
 
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully. <br>";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
+// Last ID
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+if ($conn->query($sql) === TRUE) {
+    $last_id = $conn->insert_id;
+    echo "New record created successfully. Last inserted ID is: " .
+        $last_id . "<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Select data
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"] . " - Name: " . $row["firstname"] . " " .
+            $row["lastname"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
