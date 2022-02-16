53a4a91454d189d62bc6b18bf235617b396aa7be
Leoš Gjumija <gjumle@protonmail.com>
2022-02-16 17:02:47 +0100

    Create master.js

---
+++ dev/name_list/master.js
@@ -0,0 +1,8 @@
+function empty_check() {
+    let text_in = document.getElementById("text_in").value;
+    //let number_in = document.getElementById("in_number").value;
+
+    if (/*number_in == false ||*/ text_in == false) {
+        window.alert("Fill out the fields!");
+    }
+}
