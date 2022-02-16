bdfa541a24f6bd24b0361cc5934b7171831adf4f
Leoš Gjumija <gjumle@protonmail.com>
2022-02-16 17:02:49 +0100

    Update index.php

--- dev/name_list/index.php
+++ dev/name_list/index.php
@@ -14,12 +14,21 @@ echo "Connected successfully.";
 
 
 
+
+
 ?>
 
 <html lang="en">
+<head>
+    <script src="master.js"></script>
+</head>
+<body>
 <form action="" method="post">
     <label for="text_in">Insert name: </label>
-    <input type="text" name="text_in">
-    <input type="submit">
+    <input type="text" name="text_in" id="text_in">
+    <input type="submit" onclick="empty_check()">
 </form>
+</body>
+
 </html>
+
