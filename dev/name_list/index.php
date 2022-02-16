cdbe949245850fbd0947a02d2faf0caf5aedf82b
Leoš Gjumija <gjumle@protonmail.com>
2022-02-16 17:26:15 +0100

    Update index.php

--- dev/name_list/index.php
+++ dev/name_list/index.php
@@ -9,12 +9,33 @@ $conn = new mysqli($hostname, $username, $password, $db);
 
 if ($conn->connect_error) {
     printf("Connection failed: " . $conn->connect_error);
+} else {
+    printf("Connected successfully. <br>");
 }
-echo "Connected successfully.";
 
+$name = isset($_POST["text_in"]) ? $_POST["text_in"] : "";
 
+$create_table = "CREATE TABLE IF NOT EXISTS names (
+    n_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL DEFAULT '',
+    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )";
 
+if ($conn->query($create_table) === TRUE) {
+    printf("Table names created successfully. <br>");
+} else {
+    printf("Error creating table: " . $conn->error);
+}
 
+$insert = "INSERT INTO names (name) VALUES ($name)";
+
+if ($conn->query($insert) === TRUE) {
+    printf("New record created successfully. <br>");
+} else {
+    printf("Error: " . $insert . "<br>" . $conn->error);
+}
+
+$conn->close();
 
 ?>
 
