48307980c448db6f6349d497c70e04a9691cc7f1
Leoš Gjumija <gjumle@protonmail.com>
2022-02-17 12:12:02 +0100

    Update functions.php

--- dev/name_list/functions.php
+++ dev/name_list/functions.php
@@ -10,7 +10,11 @@ function db_conn($db, $host) {
     if ($conn->connect_error) {
         echo "Connection error: " . $conn->connect_error . "<br>";
     } else {
-        echo "Connection successfull. <br>";
+        echo "Connected successfully. <br>";
     }
     return $conn;
+}
+
+function db_create_table($name, $id, $r1, $r2, $r3) {
+    $sql = "CREATE TABLE IF NOT EXISTS " . $name . "(" . $id . "INT NOT NULL AUTO_INCREMENT PRIMARY KEY,";
 }
