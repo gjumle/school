d09819a7dc11df68d45b5665834dca00bc4aaa69
Leoš Gjumija <gjumle@protonmail.com>
2022-02-17 20:33:24 +0100

    Update index.php

--- dev/name_list/index.php
+++ dev/name_list/index.php
@@ -34,7 +34,7 @@ $result = $conn->query($select);
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"] . " name: " . $row["name"]. "<br>";
+        echo "id: " . $row["id"] . " - Name: " . $row["name"]. "<br>";
     }
 } else {
     echo "Empty set";
