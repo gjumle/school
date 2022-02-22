477dc4a43b06def3a156d428413413efefa383ea
Leoš Gjumija <gjumle@protonmail.com>
2022-02-22 08:44:51 +0100

    Create functions.php

---
+++ dev/person_save/functions.php
@@ -0,0 +1,14 @@
+<?php
+
+function db_conn($db, $host) {
+    $hostname = $host;
+    $username = "root";
+    $password = "";
+
+    $conn = new mysqli($hostname, $username, $password, $db);
+
+    if ($conn->connect_error) {
+        echo "Connection error: " . $conn->connect_error . "<br>";
+    }
+    return $conn;
+}
