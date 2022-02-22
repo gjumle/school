fd4b7c12f3b249879db5bac92c209bf17e8a1236
Leoš Gjumija <gjumle@protonmail.com>
2022-02-22 08:44:54 +0100

    Create index.php

---
+++ dev/person_save/index.php
@@ -0,0 +1,52 @@
+<?php
+
+include "functions.php";
+
+$conn = db_conn("city", "localhost");
+
+$name = isset($_POST["name"]);
+$city = isset($_POST["city"]);
+$sql = "INSERT INTO persons (name, city) VALUES (" . "$name" . "," . "$city" . ") LIMIT 1";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Data save successful.";
+} else {
+    echo "Data save error: " . $conn->errno();
+}
+
+
+?>
+
+<html lang="en">
+<head>
+    <title>index</title>
+    <script src="https://code.jquery.com/jquery-3.6.0.js">
+        $(document).ready(function ()) {
+            $("#btn_save").on("clieck", function ()) {
+                var name = $("#name").val();
+                var city = $("#city").val();
+                if(name!="" && city!="") {
+                    $("#btn_save").attrChange("disabled", "disabled");
+                    
+                }
+            }
+        }
+    </script>
+</head>
+<head>
+    <div style="margin: auto; width: 50%">
+        <div id="success" style="display: none"></div>
+        <form method="post" id="form1" name="form1">
+            <input type="text" name="name" id="name" placeholder="your name">
+            <select name="city" id="city">
+                <option value="">--- city ---</option>
+                <option value="Praha">Praha</option>
+                <option value="Brno">Brno</option>
+                <option value="Bratislava">Bratislava</option>
+                <option value="Berlin">Berlin</option>
+            </select>
+            <input type="button" value="Save to DB" id="btn_save" name="btn_save">
+        </form>
+    </div>
+</head>
+</html>
