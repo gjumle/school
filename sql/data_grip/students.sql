06ed64ba2211c15d4f04072436de11f2f04705a8
Leoš Gjumija <gjumle@protonmail.com>
2022-02-24 10:17:50 +0100

    Update students.sql

--- sql/data_grip/students.sql
+++ sql/data_grip/students.sql
@@ -2,16 +2,19 @@ SHOW DATABASES;
 
 CREATE DATABASE students;
 
+DROP TABLE students;
+
 CREATE TABLE students
 (
     s_id     INT          NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name     VARCHAR(255) NOT NULL DEFAULT '',
     sur_name VARCHAR(255) NOT NULL DEFAULT '',
-    class_id INT          NOT NULL
+    class_id INT          NOT NULL,
+    CONSTRAINT s2c FOREIGN KEY (class_id) REFERENCES class(c_id)
 );
 
 CREATE TABLE class
 (
     c_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name INT NOT NULL DEFAULT ''
+    name VARCHAR(255) NOT NULL DEFAULT ''
 );
