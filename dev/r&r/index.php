a742aab4d2ce2b19dea58b3f074abd5557bc4b4e
Leoš Gjumija <gjumle@protonmail.com>
2022-02-27 20:48:30 +0100

    r&r basic index site

--- dev/r&r/index.php
+++ dev/r&r/index.php
@@ -1,1 +1,34 @@
 <?php
+
+
+?>
+
+<html>
+<head>
+	<!-- Meta -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Stylesheet -->
+	<link rel="stylesheet" type="text/css" href="./styles.css">
+	<!-- Fonts -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet"> 
+	<title>r&r</title>
+</head>
+<body>
+	<header>
+		<nav class="nav">
+			<ul class="nav-links">
+				<li><a class="nav-link" href="#">Home</a></li>
+				<li><a class="nav-link" href="#">Records</a></li>
+				<li><a class="nav-link" href="#">Stats</a></li>
+				<li><a class="nav-link nav-link-right" href="#">Account</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main class="main">
+		<h1>Comming soon!</h1>
+	</main>
+</body>
+</html>
