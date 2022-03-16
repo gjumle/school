d7b625a0851563ef6b9b804c38f0dbc8439eb69d
Leoš Gjumija <gjumle@protonmail.com>
2022-03-16 20:04:25 +0100

    Update functions.php
    Procedural, bc OOP idk

--- dev/r&r/functions.php
+++ dev/r&r/functions.php
@@ -1,7 +1,7 @@
 <?php 
 
 function db_conn($hostname, $username, $password, $db) {
-    $conn = new mysqli($hostname, $username, $password, $db);
+    $conn = mysqli_connect($hostname, $username, $password, $db);
 
     if ($conn->connect_error) {
         echo "Connection error: " . $conn->connect_error . "<br>";
@@ -21,8 +21,8 @@ function error_n($theme) {
 
 function insert_user($username) {
 	global $conn;
-	$insert_user = "INSERT INTO users (user_name) VALUES ('" . $username . "')";
-	if ($conn->query($insert_user)) {
+	$sql = "INSERT INTO users (user_name) VALUES ('" . $username . "')";
+	if (mysql_query($conn, $sql)) {
 		return success('User creation');
 	} else {
 		return error_n('User creation');
@@ -32,9 +32,9 @@ function insert_user($username) {
 function get_user($username) {
 	global $conn;
 	$sql = "SELECT u_id FROM users WHERE user_name ='" . $username . "'";
-		$result = $conn->query($sql);
-		if ($result->num_rows > 0) {
-			while ($row = $result->fetch_assoc()) {
+		$result = mysql_query($conn, $sql);
+		if (mysqli_num_rows($result) > 0) {
+			while ($row = mysqli_fetch_assoc($result)) {
 				$user_id = $row["u_id"];
 				return $user_id;
 			}
@@ -45,8 +45,8 @@ function get_user($username) {
 
 function insert_data($distance, $str_time, $user_id) {
 	global $conn;
-	$insert_data = "INSERT INTO records (distance, time_rec, user_id) VALUES (" . $distance . ", '" . $str_time . "', '" . $user_id . "')";
-	if ($conn->query($insert_data)) {
+	$sql = "INSERT INTO records (distance, time_rec, user_id) VALUES (" . $distance . ", '" . $str_time . "', '" . $user_id . "')";
+	if (mysql_query($conn, $sql)) {
 		return success('Data insert');
 	} else {
 		return error_n('Data insert');
@@ -56,7 +56,7 @@ function insert_data($distance, $str_time, $user_id) {
 function get_records() {
 	global $conn;
 	$sql = "SELECT r_id ID, distance Distance, time_rec Time, user_name Username FROM records r JOIN users u ON r.user_id=u.u_id";
-	$result = $conn->query($sql);
+	$result = mysql_query($conn, $sql);
 	echo "<div class='outputs' style='display: flex; justify-content: center; align-items: center; height: 50vh'>";
 	echo "<table class='table' style='border: 1px solid black; width: 60vw; border-collapse: collapse; margin: auto; text-align: center'>";
 	echo "<tr class='output'><th style='border: 1px solid black; padding: 14px 16px;'>ID</th>";
@@ -64,8 +64,8 @@ function get_records() {
 	echo "<th style='border: 1px solid black; padding: 14px 16px;'>Time (HH:MM:SS)</th>";
 	echo "<th style='border: 1px solid black; padding: 14px 16px;'>Username</th>";
 	echo "<th style='border: 1px solid black; padding: 14px 16px; colspan='2';'>Modify</th></tr>";
-	if ($result->num_rows > 0) {
-		while ($row = $result->fetch_assoc()) {
+	if (mysqli_num_rows($result) > 0) {
+		while ($row = mysqli_fetch_assoc($result)) {
 			echo "<tr class='output'><td style='border: 1px solid black; padding: 7px 8px;'>". $row["ID"] . "</td><td style='border: 1px solid black; padding: 7px 8px;'>" . $row["Distance"] . "</td><td style='border: 1px solid black; padding: 7px 8px;'>" . $row["Time"] . "</td><td style='border: 1px solid black; padding: 7px 8px;'>" . $row["Username"] . "</td><td style='border: 1px solid black; padding: 7px 8px;'><a style='color: green;' href='?edit_id=" . $row["ID"] . "'>Edit</a></td><td style='border: 1px solid black; padding: 7px 8px;'><a style='color: red;' onclick ='delete_check()' href='?delete_id=" . $row["ID"] . "''>Delete</a></td></tr>";
 		}
 		echo "</table></div>";
@@ -77,9 +77,9 @@ function get_records() {
 function get_record($r_id) {
 	global $conn;
 	$sql = 'SELECT r_id ID, distance Distance, time_rec Time, user_name Username FROM records r JOIN users u ON r.user_id=u.u_id WHERE r_id =' . $r_id;
-	$result = $conn->query($sql);
-	if ($result->num_rows > 0) {
-		while ($row = $result->fetch_assoc()) {
+	$result = mysql_query($conn, $sql);
+	if (mysqli_num_rows($result) > 0) {
+		while ($row = mysqli_fetch_assoc($result)) {
 			$distance = $row['Distance'];
 			$str_time = $row['Time'];
 			$username = $row['Username'];
@@ -92,7 +92,7 @@ function get_record($r_id) {
 function delete_record($r_id) {
 	global $conn;
 	$sql = 'DELETE FROM records WHERE r_id =' . $r_id . ' LIMIT 1';
-	if ($conn->query($sql)) {
+	if (mysqli_query($connect, $sql)) {
 		$get_records();
 		return success('Data delete');
 	} else {
@@ -103,8 +103,7 @@ function delete_record($r_id) {
 function update_record($r_id, $distance, $str_time, $user_id) {
 	global $conn;
 	$sql = 'UPDATE records SET distance =' . $distance . ', str_time ="' . $str_time . '", username ="' . $user_id . '" WHERE r_id =' . $r_id;
-	echo $sql;
-	if ($conn->query($sql)) {
+	if (mysql_query($conn, $sql)) {
 		$get_records();
 		return success('Data update');
 	} else {
