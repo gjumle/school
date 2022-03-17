3bc71c295fa3d2bf061552d7f2621f79ec7406bd
Leoš Gjumija <gjumle@protonmail.com>
2022-03-17 13:00:59 +0100

    hodne

--- dev/demo/index.php
+++ dev/demo/index.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect('localhost', 'r_admin', 'runrecord', 'rr');
+$conn = mysqli_connect('localhost', 'demo', 'demo', 'demo');
 
 if (!$conn) {
     die("Connection failed.");
@@ -8,7 +8,7 @@ if (!$conn) {
 
 echo "Connection succesfull.";
 
-$site = array('home', 'records', 'about');
+$site = array('input', 'records', 'about');
 
 echo "<ul>";
 
@@ -18,7 +18,7 @@ foreach ($site as $item) {
 
 echo "</ul>";
 
-$form = array('distance', 'time', 'user');
+$form = array('distance', 'time', 'username');
 
 echo "<form action='' method='post'>";
 
@@ -26,5 +26,32 @@ foreach ($form as $input) {
     echo "<input type='text' id=" . $input . " name=" . $input . " placeholder=" . $input . ">";
 }
 
-echo "<input type='submit' name='submit' id='submit'";
+echo "<input type='submit' name='submit' id='submit'>";
 echo "</form>";
+
+if (isset($_POST['distance']) && isset($_POST['time']) && isset($_POST['username'])) {
+    $distance = $_POST['distance'];
+    $time = $_POST['time'];
+    $username = $_POST['username'];
+
+    $sql = 'INSERT INTO records (distance, time, username) VALUES ("' . $distance . '", "' . $time . '", "' . $username . '")';
+    if (mysqli_query($conn, $sql)) {
+        echo "Data insert successful.";
+    } else {
+        echo "Data insert error : " . mysqli_error($conn);
+    }
+
+    $sql = 'SELECT * FROM records';
+    $result = mysqli_query($conn, $sql);
+    if (mysqli_fetch_assoc($result)) {
+        echo "<table style='border: 1px solid black;'>";
+        echo "<tr><th>ID</th><th>Distance</th><th>Time</th><th>Usernname</th></tr>";
+        while ($row = mysqli_fetch_array($result)) {
+            echo "<tr><td>" . $row['id'] . "</td>
+                  <td>" . $row['distance'] . "</td>
+                  <td>" . $row['time'] . "</td>
+                  <td>" . $row['username'] . "</td></tr>";
+        }
+        echo "</table>";
+    }
+}
