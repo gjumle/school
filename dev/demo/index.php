9f067f9748dfcc88d82cdfa53b0675c125b41c10
Leoš Gjumija <gjumle@protonmail.com>
2022-03-17 12:02:21 +0100

    Update index.php

--- dev/demo/index.php
+++ dev/demo/index.php
@@ -10,6 +10,21 @@ echo "Connection succesfull.";
 
 $site = array('home', 'records', 'about');
 
+echo "<ul>";
+
 foreach ($site as $item) {
     echo "<li><a href='?page=" . $item. "'>" . $item . "</a></li>";
 }
+
+echo "</ul>";
+
+$form = array('distance', 'time', 'user');
+
+echo "<form action='' method='post'>";
+
+foreach ($form as $input) {
+    echo "<input type='text' id=" . $input . " name=" . $input . " placeholder=" . $input . ">";
+}
+
+echo "<input type='submit' name='submit' id='submit'";
+echo "</form>";
