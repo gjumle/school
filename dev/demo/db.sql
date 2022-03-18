20640d24b317f2135cc0418db5eda6092c13737c
Leoš Gjumija <gjumle@protonmail.com>
2022-03-18 18:47:26 +0100

    Update db.sql

--- dev/demo/db.sql
+++ dev/demo/db.sql
@@ -1,1 +1,6 @@
-create table records (id int not null auto_increment primary key, distance varchar(25) not null, time varchar(25) not null, username varchar(25) not null);
+create table records (
+    id int not null auto_increment primary key,
+    distance varchar(25) not null,
+    time varchar(25) not null,
+    username varchar(25) not null
+);
