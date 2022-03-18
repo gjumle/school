50be36e26a8a3c867d8c34f8417b23d18bb29d1c
Leoš Gjumija <gjumle@protonmail.com>
2022-03-18 21:43:06 +0100

    Update master.js

--- dev/demo/master.js
+++ dev/demo/master.js
@@ -6,11 +6,25 @@ function delete_ok() {
 function empty_check() {
     let values = [];
     let empty = true;
+    let value = '';
     values.pop(document.getElementById('distance').value);
     values.pop(document.getElementById('time').value);
     values.pop(document.getElementById('username').value);
     for (let i = 0; i < values.length; i++) {
         if (values[i] != "") {
+            empty = false;
+            switch(i) {
+                case 1:
+                    getElementById('distance').style.color = 'red';
+                    break;
+                case 2:
+                    getElementById('time').style.color = 'red';
+                    break;
+                case 3:
+                    getElementById('usernames').style.color = 'red';
+                    break;
+            }
+        } else {
             empty = false;
         }
     }
