9659515aa154541dcc22a967711aaa379d227a6f
Leoš Gjumija <gjumle@protonmail.com>
2022-05-15 21:54:55 +0200

    results order

--- dev/r&r/sql/stats.sql
+++ dev/r&r/sql/stats.sql
@@ -2,19 +2,28 @@ CREATE VIEW web_stats_sum_by_user AS
     SELECT SUM(distance_name) Distance, user_name Username FROM records r
         JOIN users u ON r.user_id=u.u_id
         JOIN distance d ON r.distance_id=d.d_id
-        GROUP BY Username;
+        GROUP BY Username
+        ORDER BY Distance DESC;
+
+DROP VIEW web_stats_sum_by_user;
 
 CREATE VIEW web_stats_avg_by_user AS
     SELECT avg(distance_name) Distance, user_name Username FROM records r
         JOIN users u ON r.user_id=u.u_id
         JOIN distance d ON r.distance_id=d.d_id
-        GROUP BY Username;
+        GROUP BY Username
+        ORDER BY Distance DESC;
+
+DROP VIEW web_stats_avg_by_user;
 
 CREATE VIEW web_stats_count_by_user AS
     SELECT count(distance_name) Distance, user_name Username FROM records r
         JOIN users u ON r.user_id=u.u_id
         JOIN distance d ON r.distance_id=d.d_id
-        GROUP BY Username;
+        GROUP BY Username
+        ORDER BY Distance DESC;
+    
+DROP VIEW web_stats_count_by_user;
 
 INSERT INTO records (distance_id, time_rec, user_id) VALUES
     ( 5, '01:10:00', 1),
@@ -26,4 +35,14 @@ INSERT INTO records (distance_id, time_rec, user_id) VALUES
     ( 4, '00:50:00', 2),
     ( 3, '00:40:00', 2),
     ( 2, '00:30:00', 2),
-    ( 1, '00:20:00', 2);
+    ( 1, '00:20:00', 2),
+    ( 5, '01:10:00', 3),
+    ( 4, '00:50:00', 3),
+    ( 3, '00:40:00', 3),
+    ( 2, '00:30:00', 3),
+    ( 1, '00:20:00', 3),
+    ( 5, '01:10:00', 4),
+    ( 4, '00:50:00', 4),
+    ( 3, '00:40:00', 4),
+    ( 2, '00:30:00', 4),
+    ( 1, '00:20:00', 4);
